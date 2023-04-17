4f52e0a6008fd5539b2d267b44f3a52f0e84b35c
Chungman Park <chungmanpark@Chungmanui-MacBookPro.local>
2023-04-17 13:49:17 +0900

    첫번째 커밋

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<main>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</main>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
